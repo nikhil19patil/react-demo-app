a94357549f3696ef25f7cfc3973ed7afbd76acf6
Nikhil <nikhil.patil@joshsoftware.com>
2020-04-01 10:18:49 +0530

    use reducer and yup for form state and validation

--- src/components/CenterDivComponent.js
+++ src/components/CenterDivComponent.js
@@ -1,9 +1,8 @@
-import React, { Component, useReducer } from "react";
+import React, { useReducer } from "react";
 import '../css/Main.css'
 import { Button, Form, FormGroup, Label, Input, Badge} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as yup from 'yup';
-import { string } from "prop-types";
 
 const initialState = {
     email : '',
@@ -14,52 +13,79 @@ const loginActions = {
     setLoginData: "SET_LOGIN_DATA"
 };
 
-// function loginReducer(state, action) {
-//     switch (action.type) {
-//       case loginActions.setLoginData:
-//         return { ...state, ...action.payload };
-//       default:
-//         throw new Error();
-//     }
-// }
-
-// const [state, dispatch] = useReducer(loginReducer, initialState);
-
-let validationSchema = yup.object().shape({
-    email: yup.string().email('Email is invalid').required("Email is required"), 
-    password: yup.string().required('password is required'),
-});
-
-const onSubmit = e => {
-    // e.preventDefault();
-    // validationSchema.isValid(state).then(isValid => {
-    //     console.log("Login data validated: ", isValid);
-    // });
+const setLoginData = param => {
+    return {
+    type: loginActions.setLoginData,
+    payload: param
+    };
 };
 
-class CenterDivComponent extends Component {
-
-    render() {
-        return (
-            <div class="CenterDiv">
-                <Form onSubmit={onSubmit}>
-                    <FormGroup>
-                        <Label for="myEmail">Email</Label>
-                        <Input class="form-control" type="email" name="email" id="myEmail" placeholder="Enter your email" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="myPass">Password</Label>
-                        <Input type="password" name="password" id="myPass" placeholder="Enter password"/> 
-                    </FormGroup>
-                    <FormGroup style={{display: 'flex', justifyContent: 'space-between', alignContent: 'center'}}>
-                        <Button color="primary" type="submit">Login</Button>{' '}
-                        <Badge href="#" color="light">Forgot Password</Badge>
-                    </FormGroup>
-                    <Badge href="#" color="light">Not a user? Sign in.</Badge>
-                </Form>
-            </div>
-        );
+const loginReducer = (state, action) => {
+    switch (action.type) {
+      case loginActions.setLoginData:
+        return { ...state, ...action.payload };
+      default:
+        return state;
     }
+}
+
+function CenterDivComponent(props) {
+
+    const [state, dispatch] = useReducer(loginReducer, initialState);
+
+    const onTextChange = e => {
+        const { value, name } = e.target;
+        let formData = { ...state };
+        formData[name] = value;
+        dispatch(setLoginData(formData));
+    };
+
+    let validationSchema = yup.object().shape({
+        email: yup.string().email('Email is invalid').required("Email is required"), 
+        password: yup.string().required('password is required'),
+    });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        validationSchema.isValid(state).then(isValid => {
+            alert("Is login data valid: " + isValid + 
+            "\nEmail: " + ((state.email == '') ? " required" : state.email) +
+             "\nPassword: " + ((state.password == '') ? " required" : state.password));
+        });
+    };
+
+    return (
+        <div class="CenterDiv">
+            <Form onSubmit={onSubmit}>
+                <FormGroup>
+                    <Label for="myEmail">Email</Label>
+                    <Input 
+                        class="form-control" 
+                        type="email" 
+                        name="email" 
+                        id="myEmail" 
+                        placeholder="Enter your email" 
+                        onChange={onTextChange}
+                        value={state.email} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="myPass">Password</Label>
+                    <Input 
+                        type="password" 
+                        name="password" 
+                        id="myPass" 
+                        placeholder="Enter password" 
+                        onChange={onTextChange}
+                        value={state.password} /> 
+                </FormGroup>
+                <FormGroup style={{display: 'flex', justifyContent: 'space-between', alignContent: 'center'}}>
+                    <Button color="primary" type="submit">Login</Button>{' '}
+                    <Badge href="#" color="light">Forgot Password</Badge>
+                </FormGroup>
+                <Badge href="#" color="light">Not a user? Sign in.</Badge>
+            </Form>
+        </div>
+    );
 }
 
 export default CenterDivComponent
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import InputComponent from './components/InputComponent';
 import CenterDivComponent from './components/CenterDivComponent'
 import './index.css';
 
